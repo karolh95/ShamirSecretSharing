7a0504804464a4b5fd1af8aa989d8b22ae18a2ce
karolh95 <karolh95@live.com>
2020-11-13 12:35:22 +0100

    Added LagrangeInterpolation test

---
+++ src/test/java/karolh95/LagrangeInterpolationTest.java
@@ -0,0 +1,45 @@
+package karolh95;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+public class LagrangeInterpolationTest {
+
+    @Test
+    public void testSolve() {
+
+        BigInteger secret = BigInteger.valueOf(11);
+
+        BigInteger p = BigInteger.valueOf(13);
+
+        int threshold = 3;
+
+        BigInteger[] inputs = {
+                BigInteger.valueOf(1),
+                BigInteger.valueOf(2),
+                BigInteger.valueOf(3),
+                BigInteger.valueOf(4),
+                BigInteger.valueOf(5)
+        };
+        BigInteger[] outputs = {
+                BigInteger.valueOf(0),
+                BigInteger.valueOf(3),
+                BigInteger.valueOf(7),
+                BigInteger.valueOf(12),
+                BigInteger.valueOf(5)
+        };
+
+        LagrangeInterpolation interpolation = LagrangeInterpolation.builder()
+                .p(p)
+                .threshold(threshold)
+                .inputs(inputs)
+                .outputs(outputs)
+                .build();
+
+        BigInteger a0 = interpolation.solve(BigInteger.ZERO);
+
+        Assert.assertEquals("a0 should be equal to secret", secret, a0);
+    }
+}
