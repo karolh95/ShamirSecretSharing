c9470c7a32dce40afa1be62cd5813c22a0947320
karolh95 <karolh95@live.com>
2020-11-13 12:35:39 +0100

    Added RandomPolynomial test

---
+++ src/test/java/karolh95/RandomPolynomialTest.java
@@ -0,0 +1,49 @@
+package karolh95;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class RandomPolynomialTest {
+
+    @Test
+    public void applyTest() {
+
+        BigInteger p = BigInteger.valueOf(13);
+        int threshold = 3;
+
+        RandomPolynomial polynomial = new RandomPolynomial(p, threshold);
+
+        polynomial.setCoefficient(0, BigInteger.valueOf(11));
+        polynomial.setCoefficient(1, BigInteger.valueOf(8));
+        polynomial.setCoefficient(2, BigInteger.valueOf(7));
+
+        BigInteger[] inputs = {
+                BigInteger.valueOf(1),
+                BigInteger.valueOf(2),
+                BigInteger.valueOf(3),
+                BigInteger.valueOf(4),
+                BigInteger.valueOf(5)
+        };
+
+        BigInteger[] expectedOutputs = {
+                BigInteger.valueOf(0),
+                BigInteger.valueOf(3),
+                BigInteger.valueOf(7),
+                BigInteger.valueOf(12),
+                BigInteger.valueOf(5)
+        };
+
+        BigInteger[] outputs = {
+                polynomial.apply(inputs[0]),
+                polynomial.apply(inputs[1]),
+                polynomial.apply(inputs[2]),
+                polynomial.apply(inputs[3]),
+                polynomial.apply(inputs[4])
+        };
+
+        assertArrayEquals("Output should equals to expected one", expectedOutputs, outputs);
+    }
+}
