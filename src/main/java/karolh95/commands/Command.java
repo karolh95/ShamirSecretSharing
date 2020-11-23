8ba89e4f601ba815b25a834848557c2ae3e84d4d
karolh95 <karolh95@live.com>
2020-11-23 13:36:28 +0100

    Added Command interface and default NullCommand implementation

---
+++ src/main/java/karolh95/commands/Command.java
@@ -0,0 +1,6 @@
+package karolh95.commands;
+
+public interface Command {
+
+    void execute();
+}
---
+++ src/main/java/karolh95/commands/NullCommand.java
@@ -0,0 +1,9 @@
+package karolh95.commands;
+
+public class NullCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
