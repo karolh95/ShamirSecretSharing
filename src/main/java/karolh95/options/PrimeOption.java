921b0ec9b120e6c5e998f8dddf4b0afe840baf78
karolh95 <karolh95@live.com>
2020-11-23 14:54:58 +0100

    Secret and Prime are formatted as decimal numbers

--- src/main/java/karolh95/options/PrimeOption.java
+++ src/main/java/karolh95/options/PrimeOption.java
@@ -7,7 +7,7 @@ public class PrimeOption extends Option {
     public static final String longOpt = "prime";
     private static final String opt = "p";
     private static final Boolean hasArg = true;
-    private static final String description = "prime number in hexadecimal format, greater than secret";
+    private static final String description = "prime number, greater than secret";
 
     public PrimeOption() {
         super(opt, longOpt, hasArg, description);
--- src/main/java/karolh95/options/SecretOption.java
+++ src/main/java/karolh95/options/SecretOption.java
@@ -7,7 +7,7 @@ public class SecretOption extends Option {
     public static final String longOpt = "secret";
     private static final String opt = "s";
     private static final Boolean hasArg = true;
-    private static final String description = "secret number in hexadecimal format";
+    private static final String description = "secret number";
 
     public SecretOption() {
         super(opt, longOpt, hasArg, description);
