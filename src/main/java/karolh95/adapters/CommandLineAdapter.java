1e1c8d29b6b61f2b33174e59d06559f0be99b66f
karolh95 <karolh95@live.com>
2020-11-25 11:05:52 +0100

    Added CommandLineAdapter

---
+++ src/main/java/karolh95/adapters/CommandLineAdapter.java
@@ -0,0 +1,97 @@
+package karolh95.adapters;
+
+import karolh95.BigIntegerFactory;
+import karolh95.options.InputFileOption;
+import karolh95.options.OutputFileOption;
+import karolh95.options.PrimeBitLengthOption;
+import karolh95.options.PrimeOption;
+import karolh95.options.ReconstructionOption;
+import karolh95.options.SecretBitLengthOption;
+import karolh95.options.SecretOption;
+import karolh95.options.SharesOption;
+import karolh95.options.ThresholdOption;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.cli.CommandLine;
+
+import java.math.BigInteger;
+
+@RequiredArgsConstructor
+public class CommandLineAdapter {
+
+    private static final String ZERO = Integer.toString(0);
+    private static final String DEFAULT_OUTPUT = "output";
+
+    private final CommandLine cmd;
+
+    public boolean isReconstruction() {
+        return cmd.hasOption(ReconstructionOption.longOpt);
+    }
+
+    public String getInputFileName() {
+
+        return cmd.getOptionValue(InputFileOption.longOpt, null);
+    }
+
+    public String getOutputFileName() {
+
+        return cmd.getOptionValue(OutputFileOption.longOpt, DEFAULT_OUTPUT);
+    }
+
+    public BigInteger getPrime() {
+
+        int bitLength = getPrimeBitLength();
+
+        if (cmd.hasOption(PrimeOption.longOpt)) {
+            String value = cmd.getOptionValue(PrimeOption.longOpt);
+            return new BigInteger(value);
+
+        } else if (bitLength > 0) {
+            return BigIntegerFactory.getPrime(bitLength);
+
+        } else {
+            return BigInteger.ZERO;
+        }
+    }
+
+    public BigInteger getSecret() {
+
+        int bitLength = getSecretBitLength();
+
+        if (cmd.hasOption(SecretOption.longOpt)) {
+            String value = cmd.getOptionValue(SecretOption.longOpt);
+            return new BigInteger(value);
+
+        } else if (bitLength > 0) {
+            return BigIntegerFactory.getPrime(bitLength);
+
+        } else {
+            return BigInteger.ZERO;
+        }
+    }
+
+    public int getShares() {
+
+        return getIntValue(SharesOption.longOpt);
+    }
+
+    public int getThreshold() {
+
+        return getIntValue(ThresholdOption.longOpt);
+    }
+
+    private int getPrimeBitLength() {
+
+        return getIntValue(PrimeBitLengthOption.longOpt);
+    }
+
+    private int getSecretBitLength() {
+
+        return getIntValue(SecretBitLengthOption.longOpt);
+    }
+
+    private int getIntValue(String longOpt) {
+
+        String value = cmd.getOptionValue(longOpt, ZERO);
+        return Integer.parseInt(value);
+    }
+}
