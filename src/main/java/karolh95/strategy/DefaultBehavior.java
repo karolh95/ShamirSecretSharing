2254603a3a17259ce80c3386cfbf9c0fdaad33f1
karolh95 <karolh95@live.com>
2020-11-30 13:58:41 +0100

    The Default Behavior now displays a help message

--- src/main/java/karolh95/strategy/DefaultBehavior.java
+++ src/main/java/karolh95/strategy/DefaultBehavior.java
@@ -1,9 +1,19 @@
 package karolh95.strategy;
 
+import karolh95.options.OptionsFactory;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
 public class DefaultBehavior implements Behavior {
+
+    private static final String CMD_LINE_SYNTAX = "shamirSecretSharing";
 
     @Override
     public void run() {
 
+        Options options = OptionsFactory.getOptions();
+        HelpFormatter formatter = new HelpFormatter();
+
+        formatter.printHelp(CMD_LINE_SYNTAX, options, true);
     }
 }
