9ce4f53ceda5b172317fd7e361a50d724ba17051
karolh95 <karolh95@live.com>
2020-11-30 14:03:07 +0100

    Refactored Behavior Factory

--- src/main/java/karolh95/Main.java
+++ src/main/java/karolh95/Main.java
@@ -1,27 +1,11 @@
 package karolh95;
 
-import karolh95.options.OptionsFactory;
 import karolh95.strategy.BehaviorFactory;
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Options options = OptionsFactory.getOptions();
-        CommandLineParser parser = new BasicParser();
-        CommandLine cmd = null;
-
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-        }
-
-        BehaviorFactory.getBehavior(cmd).run();
+        BehaviorFactory.getBehavior(args).run();
     }
 }
--- src/main/java/karolh95/strategy/BehaviorFactory.java
+++ src/main/java/karolh95/strategy/BehaviorFactory.java
@@ -1,21 +1,35 @@
 package karolh95.strategy;
 
 import karolh95.adapters.CommandLineAdapter;
+import karolh95.options.OptionsFactory;
+import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class BehaviorFactory {
 
-    public static Behavior getBehavior(CommandLine cmd) {
+    public static Behavior getBehavior(String[] args) {
 
-        if (cmd == null) return new DefaultBehavior();
+        Options options = OptionsFactory.getOptions();
+        CommandLineParser parser = new BasicParser();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            return new DefaultBehavior();
+        }
 
         CommandLineAdapter adapter = new CommandLineAdapter(cmd);
 
-        if (adapter.isReconstruction()) {
-            return new SecretReconstructionBehavior(adapter);
+        if (adapter.isHelpEnabled())
+            return new DefaultBehavior();
 
-        } else {
-            return new ShareSecretBehavior(adapter);
-        }
+        return adapter.isReconstruction()
+                ? new SecretReconstructionBehavior(adapter)
+                : new ShareSecretBehavior(adapter);
     }
 }
